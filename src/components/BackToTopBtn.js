6e699f52eb6fbcb9013f8f3642ac3f608e4cf5f2
shaundvine <shaundvine@gmail.com>
2022-01-18 13:37:05 -0800

    Update index.css btn

--- src/components/BackToTopBtn.js
+++ src/components/BackToTopBtn.js
@@ -1,3 +1,5 @@
+// https://www.npmjs.com/package/react-back-to-top-button
+
 import React, { Component } from "react";
 import BackToTop from "react-back-to-top-button";
 import { FaArrowCircleUp } from "react-icons/fa";
@@ -12,10 +14,8 @@ class App extends Component {
         speed={1500}
         easing="easeInOutQuint"
       >
-        
-          <FaArrowCircleUp />
+        <FaArrowCircleUp />
       </BackToTop>
-       
     );
   }
 }
